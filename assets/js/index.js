54cd7a63b66550b07de5fa0913103d6943f8b776
Ben Gammon <bgammon86@gmail.com>
2025-04-14 20:54:06 +0100

    Implement further theme changes and finalise tocbot styling

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,6 +6,20 @@ import "../css/index.scss";
 // Import JS
 import tocBot from "./tocbot.min";
 
-
-// Call the menu and infinite scroll functions
-tocBot();
+tocbot.init({
+    // Where to render the table of contents.
+    tocSelector: '.gh-toc',
+    // Where to grab the headings to build the table of contents.
+    contentSelector: '.gh-content',
+    // Which headings to grab inside of the contentSelector element.
+    headingSelector: 'h1, h2, h3, h4',
+    // To handle fixed headers with tocbot, just pass the header offsets
+    // as options to tocbot.
+    headingsOffset: 100,
+    scrollSmoothOffset: -100,
+    // Ensure correct positioning
+    hasInnerContainers: true,
+    // Enable the URL hash to update with the proper heading ID as
+    // a user scrolls the page.
+    enableUrlHashUpdateOnScroll: false
+});
